feb6ec34720d9e8820abb513b4140e6823a65252
Sr-Okami <M_Sousa00@outlook.com>
2025-05-29 21:16:21 -0300

    mais uma tentativa de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/projeto-digital-store/',
   plugins: [react()],
-});
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+})
